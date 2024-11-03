d00832cc52baa262b03941e5b8d2be4f5c2dc01c
aliMustafa52 <alimustafa6267@gmail.com>
2024-11-03 19:17:42 +0200

    change api key

--- src/js/config.js
+++ src/js/config.js
@@ -4,4 +4,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 1.5;
-export const API_KEY = '770503f7-e87a-4d08-b8a6-c4de07fb7437';
+export const API_KEY = '84a21e68-04c7-4de6-8e84-2760d2828a0e';
